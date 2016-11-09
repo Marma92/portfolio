aa5c6e4797538eeb1af38531fe5b7d9a63f172ae
Marma <elbaz.amram@gmail.com>
2016-11-09 01:59:22 +0100

    views system templated a bit, preparing the future project structure

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 var express   = require('express');
 var app       = express();
-var port      = process.env.PORT || 8080;
+var port      = process.env.PORT || 8090;
 var morgan    = require('morgan');
 
 app.use(morgan('dev'));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-		res.render('index.ejs'); // load the index.ejs file
-});
+// routes ======================================================================
+require('./app/routes.js')(app);
 
 app.listen(port);
 console.log('The magic happens on port ' + port);

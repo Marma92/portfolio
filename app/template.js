99e0a58eb57de68b6804ecba316081acd435b956
Amram ELBAZ <a.elbaz@adexgroup.fr>
2017-12-05 00:51:10 +0100

    first shot of a working templating system

--- app/template.js
+++ app/template.js
@@ -2,7 +2,16 @@ module.exports = function(app){
 	// =====================================
 	// TEMPLATE PAGE                ========
 	// =====================================
+
 	app.get('/template', function(req, res) {
-		res.render('template.ejs'); // load the template.ejs file
+    var fs = require('fs');
+    fs.readFile('./data/template.json', 'utf8', function (err, data) {
+        if (err) {
+          console.log(err);
+        }
+        var obj = JSON.parse(data);
+				console.log(obj.pageTitle);
+        res.render('template.ejs', {obj : obj}); // load the template.ejs file
+      });
 	});
 }

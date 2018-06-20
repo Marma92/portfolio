dc85d62f508eddf844cc6b25618cef56da3e3654
Amram ELBAZ <a.elbaz@adexgroup.fr>
2018-06-20 20:50:57 +0200

    other projects webview templating implementation

--- app/projetsperso.js
+++ app/projetsperso.js
@@ -1,6 +1,14 @@
 module.exports = function(app){
 
+var fs = require('fs');
 	app.get('/projetsperso', function(req, res) {
-		res.render('projetsperso.ejs');
+		fs.readFile('./data/projetsperso.json', 'utf8', function (err, data) {
+				if (err) {
+					console.log(err);
+				}
+				var obj = JSON.parse(data);
+				console.log(obj.pageTitle);
+				res.render('projetsperso.ejs', {obj : obj});
+			});
 	});
 }

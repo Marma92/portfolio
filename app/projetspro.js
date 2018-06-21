3a4d713a30417a7a6992427709a7c113e3ba3115
Amram ELBAZ <a.elbaz@adexgroup.fr>
2018-06-21 12:24:13 +0200

    templating implentation for the professional projects part

--- app/projetspro.js
+++ app/projetspro.js
@@ -1,6 +1,14 @@
 module.exports = function(app){
 
+var fs = require('fs');
 	app.get('/projetspro', function(req, res) {
-		res.render('projetspro.ejs');
+		fs.readFile('./data/projetspro.json', 'utf8', function (err, data) {
+				if (err) {
+					console.log(err);
+				}
+				var obj = JSON.parse(data);
+				console.log(obj.pageTitle);
+				res.render('projetspro.ejs', {obj : obj});
+			});
 	});
 }

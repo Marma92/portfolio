759c5e7d542a270e0ea75bbf6b60a78bc5dae303
Amram ELBAZ <a.elbaz@adexgroup.fr>
2018-06-21 21:49:02 +0200

    the experience view is now templated (still WIP though)

--- app/experience.js
+++ app/experience.js
@@ -1,6 +1,16 @@
 module.exports = function(app){
-
+	// =====================================
+	// EXPERIENCE PAGE                ======
+	// =====================================
+var fs = require('fs');
 	app.get('/experience', function(req, res) {
-		res.render('experience.ejs');
+    fs.readFile('./data/experience.json', 'utf8', function (err, data) {
+        if (err) {
+          console.log(err);
+        }
+        var obj = JSON.parse(data);
+				console.log(obj.pageTitle);
+        res.render('experience.ejs', {obj : obj}); // load the template.ejs file
+      });
 	});
 }

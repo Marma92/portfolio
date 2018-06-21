3646e62099b34da47ec660235317ce2de48131b7
Amram ELBAZ <a.elbaz@adexgroup.fr>
2018-06-21 15:51:14 +0200

    templating the "contact me" view

--- app/contact.js
+++ app/contact.js
@@ -1,6 +1,14 @@
 module.exports = function(app){
 
+var fs = require('fs');
 	app.get('/contact', function(req, res) {
-		res.render('contact.ejs'); 
+		fs.readFile('./data/contact.json', 'utf8', function (err, data) {
+				if (err) {
+					console.log(err);
+				}
+				var obj = JSON.parse(data);
+				console.log(obj.pageTitle);
+				res.render('contact.ejs', {obj : obj});
+			});
 	});
 }

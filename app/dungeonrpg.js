396ef7f70d6af143803a38080af218178bb24ce2
Amram ELBAZ <a.elbaz@adexgroup.fr>
2018-06-24 00:16:18 +0200

    first shot of a project page template

--- app/dungeonrpg.js
+++ app/dungeonrpg.js
@@ -1,6 +1,15 @@
 module.exports = function(app){
 
+
+var fs = require('fs');
 	app.get('/realisations/dungeonrpg', function(req, res) {
-		res.render('realisations/dungeonrpg.ejs'); 
+		fs.readFile('./data/realisations/dungeonrpg.json', 'utf8', function (err, data) {
+        if (err) {
+          console.log(err);
+        }
+        var obj = JSON.parse(data);
+				console.log(obj.pageTitle);
+        res.render('realisations/dungeonrpg.ejs', {obj : obj}); // load the competences.ejs file
+      });
 	});
 }
